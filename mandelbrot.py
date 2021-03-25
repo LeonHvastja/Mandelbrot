26c220273b37e2abb84210008140e23c9e528fe2
LeonHvastja <leonhvastja@hotmail.com>
2021-03-25 16:28:13 +0100

    fixed input parameter bug

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,12 +1,12 @@
 #mandelbrot.py
 
-def rec(c, z):
+def rec(z, c):
     return z**2 + c
 
 def mandelbrotCheck(c, depth = 100, threshold = 1000):
     z = 0
     for i in range(depth):
         z = rec(z, c)
-        if(abs(z) > 1000*abs(c)):
+        if(abs(z) > threshold*abs(c)):
             return False
     return True
