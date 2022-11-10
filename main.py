9dc448c84ba21782aa75764f81d47b2ffea1afca
LeonHvastja <leonhvastja@hotmail.com>
2022-11-10 22:07:56 +0100

    changed data type for ram efficiency

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ k = int(input("Choose resolution factor. For reference 120 for HD, 160 for QHD, 
 
 def main():
     # how many iterations to check for divergence, 70+ gives good results
-    depth = int(input("Choose desired number of iterations."))
+    depth = int(input("Choose desired number of iterations:"))
 
     x = np.linspace(-2.5, 1.5, x_ratio*k)
     y = np.linspace(-9/8, 9/8, y_ratio*k)
@@ -26,6 +26,7 @@ def main():
 
     # initialize point array
     points = np.reshape([complex(x_,y_) for x_ in x for y_ in y], shape_tuple)
+    points = points.astype(np.csingle)
 
     # initialize divergence array
     divergence = np.full(shape_tuple, 0)
